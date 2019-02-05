4857c02d98e829c038838b802cb41e2733cf2717
Aoko <hejunqin233@gmail.com>
2019-02-05 14:35:24 +0800

    Initial commit

---
+++ hana-server/src/main/java/com/aoko/hana/HanaServerApplication.java
@@ -0,0 +1,14 @@
+package com.aoko.hana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanaServerApplication.class, args);
+	}
+
+}
+
---
+++ hana-server/src/main/java/com/aoko/hana/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.aoko.hana;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HanaServerApplication.class);
+	}
+
+}
+
